6667838689610edf3db40467f2378fd03517153b
Edward Lu <Chaosed0@gmail.com>
2015-01-24 12:16:11 -0500

    Add polygon component to draw polygons
    Collision detection not working yet

--- public/scripts/game.js
+++ public/scripts/game.js
@@ -1,6 +1,7 @@
 
-define(['crafty', 'jquery', 'TiledMapBuilder', 'TiledMapMocks',
-        './CollisionResolver'
+define(['crafty', 'jquery',
+        './CollisionResolver',
+        './Polygon'
     ], function(Crafty, $) {
     var self = this;
     var map;
@@ -8,7 +9,7 @@ define(['crafty', 'jquery', 'TiledMapBuilder', 'TiledMapMocks',
     var width = 800;
     var height = 600;
     var gameElem = document.getElementById('game');
-	var scale = 2.0;
+	var scale = 1.0;
 
     Crafty.init(width, height, gameElem);  			  		
                         
@@ -16,13 +17,26 @@ define(['crafty', 'jquery', 'TiledMapBuilder', 'TiledMapMocks',
                                     
         console.log("MAIN");
 
+		var wallPolygon = new Crafty.polygon([[100, 100], [200, 200], [500, 200]]);
+
+		//A wall
+		var wall = Crafty.e("2D, Canvas, Polygon, Collision, Wall")
+			.attr({x: 0, y: 0})
+            .collision(wallPolygon)
+            .polygon(wallPolygon.points, "#BB00BB");
+
+		/*var box = Crafty.e("2D, Canvas, Color, Collision, Wall")
+			.attr({x: 20, y: 20, w: 50, h: 50})
+            .collision()
+            .color(255, 255, 255);*/
+
         //Player
         var player = Crafty.e("2D, Canvas, Color, Fourway, CollisionResolver, Collision")
             .attr({ x: 0, y: 0, z: 10, w: 8, h: 8 })
 			.color(0, 0, 0)
             .fourway(2)
             .collision()
-            .collisionresolver('Edge')
+            .collisionresolver('Wall')
 			.bind("Moved", function(oldpos) {
 				Crafty.viewport.x = - (this.x - width/2.0/scale + this.w);
 				Crafty.viewport.y = - (this.y - height/2.0/scale + this.h);
--- public/scripts/main.js
+++ public/scripts/main.js
@@ -6,8 +6,6 @@ require.config({
     crafty: "../bower_components/crafty/dist/crafty",
     jquery: "../bower_components/jquery/dist/jquery",
     require: "../bower_components/requirejs/require",
-    TiledMapBuilder: "../TiledMapBuilder/tiledmapbuilder",
-    TiledMapMocks: "../TiledMapBuilder/create_mocks_module",
     util: "./Util"
   },
   packages: [
