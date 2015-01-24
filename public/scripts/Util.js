d28cfc09a45cc1d826129d772731ddd9e2818b6e
Edward Lu <Chaosed0@gmail.com>
2015-01-24 12:30:45 -0500

    Add function to get bounding box of polygon

--- public/scripts/Util.js
+++ public/scripts/Util.js
@@ -8,6 +8,32 @@ define(function() {
                 return Math.random() * min;
             }
         },
+
+        getBoundBox: function(polygon) {
+            var points = polygon.points;
+            var minPoint = {x: points[0][0], y: points[0][1]};
+            var maxPoint = {x: points[0][0], y: points[0][1]};
+            for(var i = 1; i < points.length; i++) {
+                var point = points[i];
+                if(point[0] < minPoint.x) {
+                    minPoint.x = point[0];
+                }
+                if(point[1] < minPoint.y) {
+                    minPoint.y = point[1];
+                }
+
+                if(point[0] > maxPoint.x) {
+                    maxPoint.x = point[0];
+                }
+                if(point[1] > maxPoint.y) {
+                    maxPoint.y = point[1];
+                }
+            }
+
+            return {x: minPoint.x, y: minPoint.y,
+                w: maxPoint.x - minPoint.x,
+                h: maxPoint.y - minPoint.y};
+        },
     }
 
     return exports;
