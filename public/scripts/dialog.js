51e095ea7bf8cb51e6b0ff5a750bc49668dd1fa2
Jeremy Neiman <docmarionum1@gmail.com>
2015-01-24 22:11:36 -0500

    Dialog Text types out

--- public/scripts/dialog.js
+++ public/scripts/dialog.js
@@ -27,7 +27,7 @@ define(['jquery'], function($) {
             return s;
         },
 
-        displayDialogText: function(dialog) {
+        displayDialogText: function(dialogText, i) {
             /*this.dialogText = Crafty.e("2D, Canvas, Text")
                 .attr({
                     x: -Crafty.viewport.x + width/2,
@@ -40,7 +40,8 @@ define(['jquery'], function($) {
                 	this.x = -Crafty.viewport.x + width/2;
                 	this.y = -Crafty.viewport.y + 100;
                 });*/
-			$("#dialog").html(dialog);
+			var d = dialogText.substring(0, dialogText.length-i);
+			$("#dialog").html(d);
         },
 
         currentlyPlaying: false,
@@ -48,11 +49,12 @@ define(['jquery'], function($) {
             console.log(baseFreq, baseDur);
             if (!this.currentlyPlaying) {
                 this.currentlyPlaying = true;
-                this.displayDialogText(this.generateDialog());
-                this._playDialog(baseFreq, baseDur, length);
+                var dialogText = this.generateDialog(length);
+                this._playDialog(dialogText, baseFreq, baseDur, length);
             }
         },
-        _playDialog: function(baseFreq, baseDur, length) {
+
+        _playDialog: function(dialogText, baseFreq, baseDur, length) {
             length = typeof length !== 'undefined' ? length : 20;
             baseFreq = typeof baseFreq !== 'undefined' ? baseFreq : 100;
             baseDur = typeof baseDur !== 'undefined' ? baseDur : 25;
@@ -66,11 +68,12 @@ define(['jquery'], function($) {
                 osc.frequency.value = freq;
                 osc.type = ["square", 'triangle', 'sine', 'sawtooth'][Math.floor(Math.random() *4)];
                 osc.start(0);
+                this.displayDialogText(dialogText, length);
 
                 var _this = this;
                 setTimeout(function() {
                     osc.stop(0);
-                    _this._playDialog(baseFreq, baseDur, length-1);
+                    _this._playDialog(dialogText, baseFreq, baseDur, length-1);
 
                 }, duration);
             } else {
