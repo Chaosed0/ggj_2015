3bcfb502ed6bdf0adb989a9f881ef39206c7e4e6
Ed Lu <chaosed0@gmail.com>
2016-12-10 19:23:41 -0500

    Add numeric as bower module
    Note that you have to run tools/build.sh in the numeric dir

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -7,7 +7,7 @@ require.config({
     jquery: "../bower_components/jquery/dist/jquery",
     require: "../bower_components/requirejs/require",
     PriorityQueue: "../bower_components/js-priority-queue/priority-queue",
-    numeric: "../bower_components/numeric/numeric-1.2.6",
+    numeric: "../bower_components/numeric/lib/numeric-1.2.6",
 	seedrandom: "../bower_components/seedrandom/seedrandom",
     util: "./Util",
   },
